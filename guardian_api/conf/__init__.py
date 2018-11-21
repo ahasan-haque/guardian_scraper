355c7c3df54744196570588b8ca7ce4a6ac74a3f
ahsanul08 <ahsanul.haque.ovi@gmail.com>
2018-11-21 17:43:24 +0600

    Added some env related configuration

--- guardian_api/conf/__init__.py
+++ guardian_api/conf/__init__.py
@@ -1,1 +1,2 @@
 from .services import services
+from .settings import debug, port
--- guardian_api/run.py
+++ guardian_api/run.py
@@ -1,4 +1,5 @@
 from views import ArticleByID, ArticlesByKeyword
+from conf import debug
 from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS
@@ -12,4 +13,4 @@ api.add_resource(ArticleByID, '/article/<string:article_id>')
 api.add_resource(ArticlesByKeyword, '/articles')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=debug)
