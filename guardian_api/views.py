099cc5738ef48f5476dac85e6d216de8b24f2843
ahsanul08 <ahsanul.haque.ovi@gmail.com>
2018-11-21 17:09:15 +0600

    view implemented

--- guardian_api/views.py
+++ guardian_api/views.py
@@ -1,5 +1,28 @@
+from conf import services
+from bson import ObjectId
+from flask_restful import Resource
+
+
 class ArticleByID(Resource):
-    pass
+    """
+    Retrive single article by ID
+
+    """
+
+    def get(self, article_id):
+        article_db = services.mongo.guardian.news
+
+        article = article_db.find_one(
+            {
+                '_id': ObjectId(article_id)
+            },
+            {
+                'keyword': 0
+            }
+        )
+        article['id'] = str(article.pop('_id'))
+
+        return article, 200
 
 
 class ArticlesByKeyword(Resource):
