20a4a39a3bdd9bb5192312630135ceb3a0d22231
ahsanul08 <ahsanul.haque.ovi@gmail.com>
2018-11-21 17:06:55 +0600

    Created endpoints

--- guardian_api/run.py
+++ guardian_api/run.py
@@ -1,3 +1,4 @@
+from views import ArticleByID, ArticlesByKeyword
 from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS
@@ -6,6 +7,9 @@ app = Flask(__name__, static_url_path='/static')
 
 CORS(app)
 api = Api(app)
+
+api.add_resource(ArticleByID, '/article/<string:article_id>')
+api.add_resource(ArticlesByKeyword, '/articles')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
---
+++ guardian_api/views.py
@@ -0,0 +1,6 @@
+class ArticleByID(Resource):
+    pass
+
+
+class ArticlesByKeyword(Resource):
+    pass
