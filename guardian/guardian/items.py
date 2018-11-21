512d311620cbe3379413d1f43c5fb1cde947305a
ahsanul08 <ahsanul.haque.ovi@gmail.com>
2018-11-21 16:06:44 +0600

    Basic item specification is created

--- guardian/guardian/items.py
+++ guardian/guardian/items.py
@@ -1,14 +1,13 @@
-# -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
+from scrapy.loader.processors import MapCompose, TakeFirst, Join
+from w3lib.html import remove_tags
 
 
 class GuardianItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    headline = scrapy.Field()
+    author = scrapy.Field()
+    content = scrapy.Field()
+    category = scrapy.Field()
+    sub_category = scrapy.Field()
+    url = scrapy.Field()
+    creation_date = scrapy.Field()
