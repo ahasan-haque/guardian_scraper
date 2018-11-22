5c34d9ec364918876fe3e460de260939c0a994b0
ahsanul08 <ahsanul.haque.ovi@gmail.com>
2018-11-22 09:29:36 +0600

    added tests fro utils

--- guardian_api/tests/test_utils.py
+++ guardian_api/tests/test_utils.py
@@ -1,8 +1,10 @@
 from unittest.mock import patch, Mock
-from utils import pre_process_fields
+from utils import pre_process_fields, stemmer
 
 
+@patch.object(stemmer, 'stem', lambda x: x)
 def test_pre_process_fields():
+    # test with blank values
     param_dict = {
                   'offset': 0,
                   'limit': 10,
@@ -13,3 +15,64 @@ def test_pre_process_fields():
                   'keyword': ''
                   }
     assert pre_process_fields(param_dict) == (0, 10, {})
+
+    # test keyword normalization
+    param_dict = {
+                  'offset': 0,
+                  'limit': 10,
+                  'author': 'Some One',
+                  'category': '',
+                  'sub-category': '',
+                  'date': '',
+                  'keyword': '  whiTespacecaSensiTive'
+                  }
+    assert pre_process_fields(param_dict) == (0, 10, {'author': 'Some One', 'keyword': 'whitespacecasensitive'})
+
+    # test invalid date
+    param_dict = {
+                  'offset': 0,
+                  'limit': 10,
+                  'author': '',
+                  'category': '',
+                  'sub-category': '',
+                  'date': '12-13-14',
+                  'keyword': ''
+                  }
+    assert pre_process_fields(param_dict) == (0, 10, {})
+
+    # date not in proper format
+    param_dict = {
+                  'offset': 0,
+                  'limit': 10,
+                  'author': '',
+                  'category': '',
+                  'sub-category': '',
+                  'date': '18-Dec-2018',
+                  'keyword': ''
+                  }
+    assert pre_process_fields(param_dict) == (0, 10, {})
+
+    # proper date input (%Y-%m-%d)
+    param_dict = {
+                  'offset': 0,
+                  'limit': 10,
+                  'author': '',
+                  'category': '',
+                  'sub-category': '',
+                  'date': '2018-12-10',
+                  'keyword': ''
+                  }
+    assert pre_process_fields(param_dict) == (0, 10, {'creation_date': '2018-12-10'})
+
+    #check sub-category field name check
+    param_dict = {
+                  'offset': 0,
+                  'limit': 10,
+                  'author': '',
+                  'category': '',
+                  'sub-category': 'Test Data',
+                  'date': '',
+                  'keyword': ''
+                  }
+    assert pre_process_fields(param_dict) == (0, 10, {'sub_category': 'Test Data'})
+
